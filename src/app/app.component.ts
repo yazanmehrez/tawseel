7e68d4dabed0964f5e26d0ad2dda0dfa6a973a4e
yazan_mehrez <yazan.mehrez@gmail.com>
2019-03-23 14:32:03 +0200

    nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {AppService} from './app.service';
-import {TranslateService} from '@ngx-translate/core';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
     selector: 'app-root',
@@ -8,14 +8,28 @@ import {TranslateService} from '@ngx-translate/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+    styles = {
+        bootstrap: '',
+        style: ''
+    };
 
-    constructor(public _appService: AppService) {
+    constructor(public _appService: AppService, public sanitizer: DomSanitizer) {
 
     }
 
     ngOnInit() {
         this._appService.language.subscribe(language => {
             this._appService.currentLanguage = language === 'en' ? 'en' : 'ar';
+            switch (language) {
+                case 'en':
+                    this.styles.bootstrap = 'assets/css/ltr/bootstrap.min.css';
+                    this.styles.style = 'assets/css/ltr/style.css';
+                    break;
+                case 'ar':
+                    this.styles.bootstrap = 'assets/css/rtl/bootstrap-ar.css';
+                    this.styles.style = 'assets/css/rtl/style.css';
+                    break;
+            }
         });
 
         $('#styles').remove();
