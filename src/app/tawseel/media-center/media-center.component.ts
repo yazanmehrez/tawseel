7db2c117c42d4fbaea882864abc0736146e2435b
yazan_mehrez <yazan.mehrez@gmail.com>
2019-04-14 01:35:18 +0300

    rtl

--- src/app/tawseel/media-center/media-center.component.ts
+++ src/app/tawseel/media-center/media-center.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AppService} from '../../app.service';
 
 @Component({
-  selector: 'app-media-center',
-  templateUrl: './media-center.component.html',
-  styleUrls: ['./media-center.component.css']
+    selector: 'app-media-center',
+    templateUrl: './media-center.component.html',
+    styleUrls: ['./media-center.component.css']
 })
 export class MediaCenterComponent implements OnInit {
+    appendSlider: boolean = false;
+    appendNews: boolean = false;
+    appendXslider: boolean = false;
+    videoIndex: number = 0;
+    mainSlider = [];
+    newsSlider = [];
+    xSlider = [];
+    videoSlider = [];
 
-  constructor() { }
+    constructor(public _appService: AppService) {
+    }
 
-  ngOnInit() {
-  }
+    getHomePageData() {
+        this.appendNews = false;
+        this.appendXslider = false;
+        this._appService.api.getHomePageService().subscribe(response => {
+            this.newsSlider = response['payload'][0]['RelatedEntities'];
+            this.xSlider = response['payload'][1]['RelatedEntities'];
+            this.videoSlider = response['payload'][2]['RelatedEntities'];
+        }, (error) => console.log(error), () => {
+            this.appendNews = true;
+            this.appendXslider = true;
+        });
+    }
+
+    ngOnInit() {
+        this.getHomePageData();
+    }
 
 }
